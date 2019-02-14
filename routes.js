159c5129d602198dc3cf411abd9d79f463ab7708
unknown <saar643@gmail.com>
2019-02-14 23:05:20 +0200

    fix

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const user = require('./routes/user');
+//const user = require('./routes/user');
 const auth = require('./routes/auth');
 
-router.use('/user',user);
+//router.use('/user',user);
 router.use('/auth',auth);
 
 module.exports = router;
