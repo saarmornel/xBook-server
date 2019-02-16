4e094680adceef8600bb21f2ac9489f31f7da450
unknown <saar643@gmail.com>
2019-02-16 21:01:28 +0200

    debug

--- config/passport.js
+++ config/passport.js
@@ -72,15 +72,8 @@ module.exports = function (passport) {
         secretOrKey: configAuth.secret
     },
         function (jwtPayload, cb) {
+            debug('extract jwt')
             return cb(null, jwtPayload);
-            // //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
-            // return UserModel.findOneById(jwtPayload.id)
-            //     .then(user => {
-            //         return cb(null, user);
-            //     })
-            //     .catch(err => {
-            //         return cb(err);
-            //     });
         }
     ));
 };
