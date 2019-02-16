1f6926c35d6b566407531ab9da8786576b3eb57e
unknown <saar643@gmail.com>
2019-02-16 16:07:24 +0200

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,8 @@ const debug = require('debug')('app');
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const { isLoggedIn } = require('./loggedIn');
 
-router.get('/success', isLoggedIn, (req, res) => {
+router.get('/success', (req, res) => {
     res.json({success: true, auth_token: req.user })
 });
 
