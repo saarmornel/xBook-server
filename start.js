5b51c1d5ba66b0900f1e5363d1c3b9fcde15cb33
unknown <saar643@gmail.com>
2019-02-16 14:53:54 +0200

    fix

--- start.js
+++ start.js
@@ -1,6 +1,10 @@
 // Transpile all code following this line with babel and use 'env' (aka ES6) preset.
 require('babel-register')({
-    presets: [ 'env' ]
+    presets: ['env', {
+        "targets": {
+            "browsers": ["last 2 Chrome versions"]
+        }
+    }]
 })
 
 // Import the rest of our application.
