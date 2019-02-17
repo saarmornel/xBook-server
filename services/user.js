790e4bb88bd4ddd7e9b927908c8a3351d27a4997
SirSaar <saar643@gmail.com>
2019-02-17 11:21:44 +0200

    fix: name

--- services/user.js
+++ services/user.js
@@ -29,26 +29,26 @@ module.exports = class userService {
     }
 
     static async addBook(userId, book) {
-        const userDoc = await User.findBy(userId).exec();
+        const userDoc = await User.findById(userId).exec();
         userDoc.books.push(book);
         return await userDoc.save();
     }
 
     static async findBookById(userId,bookId) {
-        const userDoc = await User.findBy(userId).exec();
+        const userDoc = await User.findById(userId).exec();
         const bookDoc = userDoc.books.id(bookId);
         return bookDoc;
     }
 
     static async updateBookById(userId, bookId, book){
-        const userDoc = await User.findBy(userId).exec();
+        const userDoc = await User.findById(userId).exec();
         const bookDoc = userDoc.books.id(bookId);
         bookDoc.set(book);
         return await userDoc.save();
     }
 
     static async deleteBookById(userId, bookId){
-        const userDoc = await User.findBy(userId).exec();
+        const userDoc = await User.findById(userId).exec();
         userDoc.books.id(bookId).remove();
         return await userDoc.save();
     }
