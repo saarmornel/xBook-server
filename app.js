8eaeb03543c03b36ba6dc92ac4ad75d3c5b034c0
SirSaar <saar643@gmail.com>
2020-01-02 13:44:37 +0200

    fix

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(function (req, res, next) {
 
   // Request headers you wish to allow
   // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   // Pass to next layer of middleware
   next();
 });
