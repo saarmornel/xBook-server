60cae1b86e24a6a23fd6fe2a083978f508ed436f
SirSaar <saar643@gmail.com>
2020-01-05 10:16:13 +0200

    refactor

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 module.exports = {
     'secret'  : 'SECRET',
     'facebookAuth' : {
-        'clientID'      : '394487584338989', // your App ID
-        'clientSecret'  : 'ad9bd92277858b2a1f26ed275fb49738', // your App Secret
+        'clientID'      : process.env.FB_CLIENT_ID, // your App ID
+        'clientSecret'  : process.env.FB_CLIENT_SECRET, // your App Secret
         'callbackURL'   : process.env.FB_CALLBACK,
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name','friends','photos'] // For requesting permissions from Facebook API
--- routes/auth.js
+++ routes/auth.js
@@ -25,10 +25,10 @@ router.get('/facebook',
 router.get('/facebook/callback',
     passport.authenticate('facebook', {
         session: false,
-        failureRedirect: 'https://xbook-web.herokuapp.com/sign_in?auth_token=null'
+        failureRedirect: `${CLIENT_URL}/sign_in?auth_token=null`
     }), (req, res) => {
         debug('callback called');
-        res.redirect(`https://xbook-web.herokuapp.com/sign_in?auth_token=${req.user.jwtoken}`)
+        res.redirect(`${CLIENT_URL}/sign_in?auth_token=${req.user.jwtoken}`)
     }
 );
 
