09a3ad4548425b1788cbc58ddd6dcd9bb8892530
SirSaar <saar643@gmail.com>
2020-02-26 10:09:28 +0200

    fix: increase results

--- config/query.js
+++ config/query.js
@@ -2,5 +2,5 @@ module.exports = {
     publicInfo : '-facebook -location -phone',
     friendInfo : '-facebook -location -phone',
     privateInfo : '-facebook -location -phone',
-    pagination : {resultsPerPage: 10}
+    pagination : {resultsPerPage: 10000}
 }
